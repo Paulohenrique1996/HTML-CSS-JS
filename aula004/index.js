b5124abad9f52f90560478d6cd169fa99a818c1c
Paulohenrique1996 <paulo.589.2015@gmail.com>
2023-03-03 15:42:41 -0300

    Create index.js

---
+++ aula004/index.js
@@ -0,0 +1,7 @@
+let numbers = [1, 2, 3]
+let frutas = ["banana", "maçã", "uva", "laranja"]
+let numbersEFrutas = [1, "banana", 3, "maçã"]
+
+console.log(numbers)
+console.log(frutas)
+console.log(numbersEFrutas)
