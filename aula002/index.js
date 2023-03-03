7ab745d892ff6486fd96cbad8cab9f37f44adea3
Paulohenrique1996 <paulo.589.2015@gmail.com>
2023-03-03 15:23:27 -0300

    create aula 002 and 003

---
+++ aula002/index.js
@@ -0,0 +1,18 @@
+/* numero */
+let x = 5;
+let y = 3;
+let z = 5;
+
+/* String */
+let w = '5';
+
+/* True ou false */
+let idadeDoPaulo = 26;
+let idadeDaLeticia = 15;
+
+let pauloPodeEntrar = idadeDoPaulo >= 18
+console.log(pauloPodeEntrar);
+
+let leticiaPodeEntrar = idadeDaLeticia >= 18;
+console.log(leticiaPodeEntrar)
+
---
+++ aula003/index.js
@@ -0,0 +1,7 @@
+let x = prompt();
+
+if (x < 6){
+    console.log("x é menor que 6")
+} else {
+    console.log("x é maior que 6")
+}
