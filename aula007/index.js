272cf27c06b8c003c6080b86b7a6c7cb9e2dc70f
Paulohenrique1996 <paulo.589.2015@gmail.com>
2023-03-04 20:13:24 -0300

    Update index.js

---
+++ aula007/index.js
@@ -0,0 +1,28 @@
+function somaDosNumeros(a, b) {
+    let resultadoSoma = a + b
+    return resultadoSoma
+}
+
+function subDosNumeros(a, b){
+    let resultadoSub = a - b
+    return resultadoSub
+}
+
+let resultado = somaDosNumeros (5, 8);
+let resultado2 = somaDosNumeros(2, 3);
+
+let resultado3 = subDosNumeros (6, 8);
+let resultado4 = subDosNumeros(7, 3);
+
+console.log(resultado);
+console.log(resultado2);
+
+console.log(resultado3);
+console.log(resultado4);
+
+
+function digaOla(){
+    alert("Ola")
+}
+
+digaOla();
