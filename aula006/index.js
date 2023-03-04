379740634e0f6d957e58a80894e767ea7a97b3c7
Paulohenrique1996 <paulo.589.2015@gmail.com>
2023-03-04 14:44:38 -0300

    Update index.js

---
+++ aula006/index.js
@@ -0,0 +1,11 @@
+let minhaXicara = {
+    cor: "Marrom",
+    peso: 30,
+};
+
+let meuPrimeiroPedido = {
+    nome: "X-Burger",
+    preco: 30,
+    disponivel: true,
+    ingredientes: ["pão", "tomate", "alface", "hamburger", "queijo"]
+};
